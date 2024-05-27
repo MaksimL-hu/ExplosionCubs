3cd1c29472d527230c3539758970c8f51c7cf40f
MaksimL-hu <137220663+MaksimL-hu@users.noreply.github.com>
2024-05-27 17:06:13 +0300

    Add files via upload

--- Cub.cs
+++ Cub.cs
@@ -1,62 +1,22 @@
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Cub : MonoBehaviour
 {
-    [SerializeField] private int _minCountCreatedCubes = 2;
-    [SerializeField] private int _maxCountCreatedCubes = 6;
-    [SerializeField] private int _decreaseScale = 2;
-    [SerializeField] private int _decreaseSeparationChance = 2;
-    [SerializeField] private float _explosionForce;
-    [SerializeField] private float _explosionRadius;
     [SerializeField] private float _separationChance = 1f;
-    [SerializeField] private Cub _prefab;
 
-    private System.Random _random;
+    public event UnityAction<Cub> Clicked;
 
-    private void Start()
-    {
-        _random = new System.Random();
-    }
+    public float SeparationChance => _separationChance;
 
     private void OnMouseUpAsButton()
     {
-        CreateCubs();
-    }
-
-    private void CreateCubs()
-    {
-        if (_separationChance > (float)_random.NextDouble())
-        {
-            int count = _random.Next(_minCountCreatedCubes, _maxCountCreatedCubes + 1);
-            List<Rigidbody> explosionObjects = new List<Rigidbody>();
-
-            for (int i = 0; i < count; i++)
-                explosionObjects.Add(InstantiateCub().GetComponent<Rigidbody>());
-
-            foreach (Rigidbody explosionObject in explosionObjects)
-                explosionObject.AddExplosionForce(_explosionForce, transform.position, _explosionRadius);
-        }
-
+        Clicked?.Invoke(this);
         Destroy(gameObject);
     }
 
-    private Cub InstantiateCub()
+    public void SetSeparationChance(float newSeparationChance)
     {
-        Cub cub = Instantiate(_prefab);
-        cub.transform.localScale /= _decreaseScale;
-        cub.GetComponent<Renderer>().material.color = GetRandomColor();
-        cub._separationChance /= _decreaseSeparationChance;
-
-        return cub;
-    }
-
-    private Color GetRandomColor()
-    {
-        float randomChannelOne = (float)_random.NextDouble();
-        float randomChannelTwo = (float)_random.NextDouble();
-        float randomChannelThree = (float)_random.NextDouble();
-
-        return new Color(randomChannelOne, randomChannelTwo, randomChannelThree);
+        _separationChance = newSeparationChance;
     }
 }
---
+++ CubSpawner.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubSpawner : MonoBehaviour
+{
+    [SerializeField] private int _minCountCreatedCubes = 2;
+    [SerializeField] private int _maxCountCreatedCubes = 6;
+    [SerializeField] private int _decreaseScale = 2;
+    [SerializeField] private int _decreaseSeparationChance = 2;
+    [SerializeField] private float _explosionForce;
+    [SerializeField] private float _explosionRadius;
+    [SerializeField] private List<Cub> _cubs;
+
+    private System.Random _random;
+
+    private void Start()
+    {
+        _random = new System.Random();
+    }
+
+    private void OnEnable()
+    {
+        foreach (Cub cub in _cubs)
+            cub.Clicked += SpawnCubs;
+    }
+
+    private void SpawnCubs(Cub cub)
+    {
+        cub.Clicked -= SpawnCubs;
+
+        if (cub.SeparationChance > (float)_random.NextDouble())
+        {
+            int count = _random.Next(_minCountCreatedCubes, _maxCountCreatedCubes + 1);
+
+            _cubs.Remove(cub);
+            List<Rigidbody> explosionObjects = new List<Rigidbody>();
+
+            for (int i = 0; i < count; i++)
+                explosionObjects.Add(InstantiateCub(cub).GetComponent<Rigidbody>());
+
+            foreach (Rigidbody explosionObject in explosionObjects)
+                explosionObject.AddExplosionForce(_explosionForce, cub.transform.position, _explosionRadius);
+        }
+    }
+
+    private Cub InstantiateCub(Cub cub)
+    {
+        Cub newCub = Instantiate(cub);
+        newCub.transform.localScale /= _decreaseScale;
+        newCub.GetComponent<Renderer>().material.color = GetRandomColor();
+        newCub.SetSeparationChance(cub.SeparationChance / _decreaseSeparationChance);
+        newCub.Clicked += SpawnCubs;
+        _cubs.Add(newCub);
+
+        return newCub;
+    }
+
+    private Color GetRandomColor()
+    {
+        float randomChannelOne = (float)_random.NextDouble();
+        float randomChannelTwo = (float)_random.NextDouble();
+        float randomChannelThree = (float)_random.NextDouble();
+
+        return new Color(randomChannelOne, randomChannelTwo, randomChannelThree);
+    }
+}
